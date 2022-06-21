05695a9337597d4d3b422997f2c6c406845b3ce5
John Weilert <mattweilert@gmail.com>
2022-06-20 21:37:28 -0500

    Fixed/New Repo

---
+++ views/home.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+const Def = require('./default')
+
+function home () {
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                    <img className="homeImg" src="/images/homePic.jpg" alt="Fruity Drink" />
+                </div>
+                <a href="/places">
+                    <button className="btn btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home 
