b7fe2f1e5f0c894c810b4a3969da287e1a5c43c7
John Weilert <mattweilert@gmail.com>
2022-06-28 17:51:09 -0500

    Caught up

--- models/places.js
+++ models/places.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
@@ -10,14 +11,12 @@ const placeSchema = new mongoose.Schema({
       type: Number,
       min: [1673, 'Surely not that old?!'],
       max: [new Date().getFullYear(), 'Hey, this year is in the future!']
-    }
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
   })
   
-  
-
 placeSchema.methods.showEstablished = function() {
     return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
-  }
-  
+}
 
 module.exports = mongoose.model('Place', placeSchema)
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,20 +1,11 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form (data) {
-    let message = ''
-    if(data.message){
-      message = (
-        <h4 className='alert-danger'>
-          {data.message}
-        </h4>
-      )
-    }
+function new_form () {
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
-                {message}
                 <form method="POST" action="/places">
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
@@ -36,14 +27,9 @@ function new_form (data) {
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" />
                     </div>
-                    <div className="form-group col-sm-4">
-                        <label htmlFor="founded">Founded Year</label>
-                        <input 
-                            type="number" 
-                            className="form-control" 
-                            id="founded" 
-                            name="founded" 
-                            value={new Date().getFullYear()} />
+                    <div className="form-group">
+                        <label for="founded">Founded Year</label>
+                        <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
